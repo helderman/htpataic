e1954dc20e8097b77cc9c255b25872078e25b068
Ruud Helderman <r.helderman@hccnet.nl>
2019-10-21 21:43:22 +0200

    Distinguish between actors and non-actors in inventory2.c

--- code14/inventory2.c
+++ code14/inventory2.c
@@ -7,25 +7,37 @@
 
 bool executeGetFrom(void)
 {
-   // TODO: report if actor
    OBJECT *from = reachableObject("where to get that from", params[1]);
    if (from != NULL && getVisible("what you want to get", params[0]) != NULL)
    {
-      moveObject(getPossession(from, "get", params[0]), player);
+      if (from->health > 0)
+      {
+         printf("You should ask %s nicely.\n", from->description);
+      }
+      else
+      {
+         moveObject(getPossession(from, "get", params[0]), player);
+      }
    }
    return true;
 }
 
 bool executePutIn(void)
 {
-   // TODO: report if actor
    OBJECT *obj = getPossession(player, "put", params[0]);
    if (obj != NULL)
    {
       OBJECT *to = reachableObject("where to put that in", params[1]);
       if (to != NULL)
       {
-         moveObject(obj, to);
+         if (to->health > 0)
+         {
+            printf("You should offer that nicely to %s.\n", to->description);
+         }
+         else
+         {
+            moveObject(obj, to);
+         }
       }
    }
    return true;
@@ -33,25 +45,40 @@ bool executePutIn(void)
 
 bool executeAskFrom(void)
 {
-   // TODO: report if not actor
    OBJECT *from = reachableObject("who to ask that", params[1]);
-   if (from != NULL && getVisible("what you want to ask", params[0]) != NULL)
+   if (from != NULL)
    {
-      moveObject(getPossession(from, "ask", params[0]), player);
+      if (from->health > 0)
+      {
+         if (getVisible("what you want to ask", params[0]) != NULL)
+         {
+            moveObject(getPossession(from, "ask", params[0]), player);
+         }
+      }
+      else
+      {
+         printf("There is no response from %s.\n", from->description);
+      }
    }
    return true;
 }
 
 bool executeGiveTo(void)
 {
-   // TODO: report if not actor (not really error; especially when dead actor)
    OBJECT *obj = getPossession(player, "give", params[0]);
    if (obj != NULL)
    {
       OBJECT *to = reachableObject("who to give that to", params[1]);
       if (to != NULL)
       {
-         moveObject(obj, to);
+         if (to->health > 0)
+         {
+            moveObject(obj, to);
+         }
+         else
+         {
+            printf("No eagerness is shown by %s.\n", to->description);
+         }
       }
    }
    return true;
